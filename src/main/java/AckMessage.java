0325a0e1c1ebc6c0ea18ad642a6eaddee32b7ebf
Nidhi Singh <N.Singh-2@student.tudelft.nl>
2013-06-09 18:36:09 +0200

    ackmsg class

---
+++ src/main/java/AckMessage.java
@@ -0,0 +1,44 @@
+package main.java;
+
+
+public class AckMessage{
+	private int count;
+	private String sender;
+	private String receiver;
+	
+	public AckMessage(int count, String sender, String receiver){
+		this.setCount(count);
+		this.setSender(sender);
+		this.setReceiver(receiver);
+
+	}
+
+	public AckMessage() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+}
